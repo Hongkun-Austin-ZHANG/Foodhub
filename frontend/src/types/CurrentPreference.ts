4ae51c3536cb62036c3ec68a2f68977295bec932
Maoyu <mao2602967159@outlook.com>
2026-08-23 06:18:03 +1000

    Delete frontend directory

--- frontend/src/types/CurrentPreference.ts
+++
@@ -1,8 +0,0 @@
-import type { SpiceLevel } from './UserPreferenceProfile'
-
-export interface CurrentPreference {
-  preferred_proteins: string[]
-  preferred_flavours: string[]
-  preferred_textures: string[]
-  spice_level: SpiceLevel
-}
